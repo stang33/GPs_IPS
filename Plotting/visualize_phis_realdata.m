658974ddcbf639e2123128b8bb4ed1aa81b0a133
jcfeng1992 <37516401+jcfeng1992@users.noreply.github.com>
2022-06-07 15:41:55 -0400

    Update visualize_phis_realdata.m

--- Plotting/visualize_phis_realdata.m
+++ Plotting/visualize_phis_realdata.m
@@ -67,6 +67,9 @@ edges_idxs =edges_idxs_fine(1:5:end);
 centers =edges(edges_idxs);
 histdata1            = rho_emp.rdens(edges_idxs(1:end));                    % this is the "true" \rhoLT from many MC simulations
 
+%% save the result posterior mean
+learnInfo.r = r;
+learnInfo.phi = phi_mean;
 
 %% plot the density of rho
 yyaxis right                                                                                % display \rho^L_T and its estimator
