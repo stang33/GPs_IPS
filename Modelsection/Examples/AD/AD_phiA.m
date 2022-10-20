ce51ae38f4a4f768ef4b1002f01650eb34c2293d
CharlesKulick1 <105407766+CharlesKulick1@users.noreply.github.com>
2022-10-20 00:32:07 -0700

    Delete Modelsection directory

--- Modelsection/Examples/AD/AD_phiA.m
+++
@@ -1,8 +0,0 @@
-function phi = AD_phiA(r)
-% function phi = AD_phiA(r, tau, U_prime)
-
-% (C)
-tau = 0.1;
-
-phi = tau * (1+r.^2).^(-0.5);
-end
--- Modelsection/TestEMM.m
+++
@@ -1,8 +0,0 @@
-ds = [1 1.5 2 2.5 3 3.5  4 4.5 5 5.5 6 6.5 7 7.5 8 ];% 22 24 29 30];
-length(ds)
-
-k = 4;%pts
-M = 5; %boxes - 1, edge case is 
-
-
-rder = EMM(ds, k, M)
