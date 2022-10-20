f4af53bfeec77202979994539834f274e17cdc53
CharlesKulick1 <105407766+CharlesKulick1@users.noreply.github.com>
2022-10-20 00:32:27 -0700

    Delete SingleKernelCase directory

--- SingleKernelCase/Generate_data/trajUnifNoiseAdditive.m
+++
@@ -1,7 +0,0 @@
-function trajData = trajUnifNoiseAdditive( trajData, sigma )
-% Adds additive gaussian noise of relative amplitude sigma to trajectories
-% (c) XXXX
-
-trajData = trajData+sigma*(randn(size(trajData)));
-
-return
