0859978ecdc560de6222e6cc8ec88f8014783fe2
CharlesKulick1 <105407766+CharlesKulick1@users.noreply.github.com>
2022-10-27 21:32:50 -0700

    Delete Accel directory

--- Accel/KForFullRExp.m
+++
@@ -1,5 +0,0 @@
-function out = KForFullRExp(x,y,omega,delta,n)
-
-    out = (1/n^2) * delta^2 * exp(-1 * omega * abs(x - y));
-
-end
--- Accel/getSpatialCoord.m
+++
@@ -1,6 +0,0 @@
-function out = getSpatialCoord(data, i, j, m, l)
-
-    d = 2; %ambient dimension of particles
-    out = data((d * i - 1) + (j - 1), l, m);
-
-end
