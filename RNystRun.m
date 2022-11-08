396c149abf3f5886e99eac99eacd6c18036987f5
CharlesKulick1 <105407766+CharlesKulick1@users.noreply.github.com>
2022-11-08 10:40:58 -0800

    Add files via upload

--- RNystRun.m
+++ RNystRun.m
@@ -4,9 +4,19 @@ function RNystRun(MVAL)
 
 addpaths;
 
-%Load data.
-firstpart = strcat("FM1011",num2str(MVAL));
-fullname = strcat(firstpart, "FixL.mat");
+%Enter method to use for greatest likelihood.
+%@GlikSTEPreConSTE52 - the full accelerated method.
+%@GlikSTEPreConSTE52EK - Use exact derivative calculations.
+%@GlikSTEPreConSTE52EFVAL - Use exact fval calculation.
+GlikMethod = @GlikSTEPreConSTE52;
+
+%Preconditioner method.
+PreconMethod = @RandomNystbackup;
+
+%Load data. Change this as needed to call your exact dataset.
+%firstpart = strcat("FM1011","5");
+%fullname = strcat(firstpart, "FixL.mat");
+fullname = "FM600";
 load(fullname);
 
 %Constants.
@@ -17,51 +27,43 @@ n = learnInfo.N;
 D = learnInfo.d;
 
 %Adjustable parameters.
-CGErrorTol = 10^(-6);
-CG_ITER_LIMIT = 150;
-mFORGLIK = CG_ITER_LIMIT;
-lFORGLIK = CG_ITER_LIMIT;
-rangeOfI = floor(M*LForDecomp*n*D / 10);
-jitter = 10^(-5);
-HVAL = 10^(-5);
-GlikRuns = 100;
-alpha = 1;
-nT = 4;     %number of trials
+CGErrorTol = 10^(-6);   %Error tolerance in the CG algorithm.
+CG_ITER_LIMIT = 150;   %Max number of CG iterations.
+mFORGLIK = CG_ITER_LIMIT;   %Number of test vectors for Lanczos.
+lFORGLIK = CG_ITER_LIMIT;   %Number of CG iters from Lanczos.
+rangeOfI = floor(M*LForDecomp*n*D / 10);   %Size of preconditioner.
+jitter = 10^(-5);   %Value to use for sigma if sigma is too small.
+HVAL = 10^(-5);   %NOT USED.
+GlikRuns = 100;   %Runs of minimizing.
+alpha = 1;  %Range of randomness in intialization of hyps.
+nT = 10;     %Number of trials.
 
-
-
-%Decomp for K_E.
+%Decomp for decompositon.
 data = learnInfo.xpath_train(1:D*n,:,:);
 dataA = learnInfo.xpath_train(D*n+1:2*D*n,:,:);
 
+%Result storage containers.
 errorphis = zeros(4,nT);      %store errors of phis in L-infinity and L2rhoT norms
 errortrajs_train = zeros(4,nT);     %store mean and std of trajectory error in training data
 errortrajs_test = zeros(4,nT);      %store mean and std of trajectory error in testing data
 hypparameters = zeros(length(learnInfo.hyp0),nT);   %store estimated hyperparameters
 errorhyp = zeros(3,nT);      %store hyperparameter errors
-runtimes = zeros(4,nT);
+runtimes = zeros(4,nT);      %store runtimes of each section
 
 
 for k = 1 : nT
 
+    %These are guesses for hyperparameters so we can center
+    %intitialization values for the learned hyperparameters.
+    originalHyps = [2.5, 1.6, 2.3, 1.2, .01, 1.5, 0.5];
 
-%     originalHyps = [1, 1, 1, 1, 0.5, 1, 1];
-%     originalHyps = 2.5 + alpha * (2 * rand(1,7) - 1);
-%     originalHyps(3) = 1.6 + alpha * (2 * rand(1,1) - 1);
-%     originalHyps(4) = 2.3 + alpha * (2 * rand(1,1) - 1);
-%     originalHyps(5) = .01 + alpha * (2 * rand(1,1) - 1);
-%     originalHyps(6) = 1.5 + alpha * (2 * rand(1,1) - 1);
-%     originalHyps(7) = 0.5 + alpha * (2 * rand(1,1) - 1);
-
-    %We use the theoretically harder starting parameters, as they seem
-    % to have a lower failure rate.
-    originalHyps = 1 + alpha * (2 * rand(1,7) - 1);    
+    %Add randomness.
+    for i = 1 : 7
+        originalHyps(i) = originalHyps(i) + alpha * (2 * rand(1,1) - 1);
+    end
     learnInfo.hyp = originalHyps;
 
-
-
-
-    %Get constants.
+    %Get constants from hyperparameters.
     deltaE = exp(learnInfo.hyp(1));
     omegaE = exp(learnInfo.hyp(2));
     deltaA = exp(learnInfo.hyp(3));
@@ -75,18 +77,17 @@ for k = 1 : nT
 
     %Time a single run of Greatest Likelihood.
     tic;
-    [fval2, dfval2,~] = GlikSTEPreConSTE52(learnInfo, learnInfo.hyp, mFORGLIK, lFORGLIK, CGErrorTol, HVAL, M, rangeOfI, @RandomNyst)
+    [fval2, dfval2,~] = GlikMethod(learnInfo, learnInfo.hyp, mFORGLIK, lFORGLIK, CGErrorTol, HVAL, M, rangeOfI, PreconMethod)
     runtimes(1,k) = toc;
 
     
     %Reset and run the actual optimization.
     learnInfo.hyp = originalHyps;
-    [fval, dfval,~] = GlikSTEPreConSTE52(learnInfo, learnInfo.hyp, mFORGLIK, lFORGLIK, CGErrorTol, HVAL, M, rangeOfI, @RandomNyst);
-    Glik_hyp = @(hyp)GlikSTEPreConSTE52(learnInfo, hyp, mFORGLIK, lFORGLIK, CGErrorTol, HVAL, M, rangeOfI, @RandomNyst);
+    [fval, dfval,~] = GlikMethod(learnInfo, learnInfo.hyp, mFORGLIK, lFORGLIK, CGErrorTol, HVAL, M, rangeOfI, PreconMethod);
+    Glik_hyp = @(hyp)GlikMethod(learnInfo, hyp, mFORGLIK, lFORGLIK, CGErrorTol, HVAL, M, rangeOfI, PreconMethod);
     [learnInfo.hyp,flik,i] = minimize(learnInfo.hyp, Glik_hyp, -GlikRuns);
     runtimes(2,k) = toc;
 
-    
     %If accelerated, then we only need Ym to predict.
     learnInfo.option = 'alldata';
     learnInfo = GetYm(learnInfo,learnInfo.hyp);
@@ -104,7 +105,7 @@ for k = 1 : nT
         sigma = jitter;
     end
     
-    
+    %Now we set up for prediction.
     X = learnInfo.X;
     dN = learnInfo.d*learnInfo.N*learnInfo.order;
     L = length(X)/dN;
@@ -119,7 +120,8 @@ for k = 1 : nT
     KA = U_sA * R_sA * U_sA';
     MultByKA = @(x) KA * x;
     
-    [~, PreConInvRaw] = RandomNyst(learnInfo, LForDecomp, M, rangeOfI, jitter, KE, KA, sigma);
+    %One more preconditioner call.
+    [~, PreConInvRaw] = PreconMethod(learnInfo, LForDecomp, M, rangeOfI, jitter, KE, KA, sigma);
     PreConInv = @(x) PreConInvRaw * x;
 
     %Multiply by K + sigmaI.
@@ -130,14 +132,12 @@ for k = 1 : nT
     
     %Matrix multiplication.
     multMatByKInv = @(X) RunCGOnMatrixInitGuesser(MultByWholeK, X, PreConInv, CGErrorTol, CG_ITER_LIMIT);
-    
     learnInfo.invKTimesYm = multByKInv(learnInfo.Ym);
 
     %Visualize kernel.
     visualize_phis_CG(sysInfo,obsInfo,learnInfo,'E', multMatByKInv);
     visualize_phis_CG(sysInfo,obsInfo,learnInfo,'A', multMatByKInv);
     runtimes(3,k) = toc;
-    
     
     %Calculate errors.
     range = [0, learnInfo.rhoLT.edges(max(find(learnInfo.rhoLT.rdens~=0)))];
@@ -149,14 +149,14 @@ for k = 1 : nT
     errortrajs_test(:,k) = [result_test.train_traj_error result_test.prediction_traj_error]';
     runtimes(4,k) = toc;
 
-    filename = strcat("RNystITER",num2str(MVAL));
+    %Save file.
+    filename = strcat("RNystITERE",num2str(MVAL));
     save(filename);
 
 end
 
 %Print results for ease of use.
 runtimes
-
 
 avgtime = zeros(4,1);
 stdtime = zeros(4,1);
@@ -168,7 +168,6 @@ end
 
 avgtime
 stdtime
-
 
 avgkerror = zeros(4,1);
 stdkerror = zeros(4,1);
@@ -192,13 +191,11 @@ avgetrain
 stdetrain
 avgetest
 stdetest
-
 hypparameters
 
 
-
-filename = strcat("RNystResult",num2str(MVAL));
-
+%Save final report.
+filename = strcat("RNystResultE",num2str(MVAL));
 save(filename);
 
 
