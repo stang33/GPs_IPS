4026b698f99b208fc97c4d75eb4d1f08ef209c16
CharlesKulick1 <105407766+CharlesKulick1@users.noreply.github.com>
2022-11-12 13:29:38 -0800

    Add files via upload

---
+++ RandomNystbackup2.m
@@ -0,0 +1,61 @@
+function [logDetPreCon, PreConInvRaw] = RandomNystbackup(learnInfo, LForDecomp, M, rangeOfI, jitter, KE, KA, sigma)
+
+    N = learnInfo.N;
+    D = learnInfo.d;
+
+    if abs(sigma) < 10^(-6)
+        sigma = jitter;
+    end
+
+    %K is our full kernel matrix, K_phiE + K_phiA.
+    K = KE + KA + jitter * eye(M*LForDecomp*N*D);
+
+    %Symmetrize.
+    K = (K + K') / 2;
+
+    %Rank parameter. rank = s. Number of columns subsampled.
+    rank = rangeOfI;
+
+    %Choose random permutation, make a permuation matrix.
+    perm = randperm(N*D*M*LForDecomp);
+    P = eye(N*D*M*LForDecomp);
+    P = P(perm,:);
+
+
+    %Initialize
+    Aplus = zeros(rank, rank);
+    
+    %Permute rows and columns to get the columns we want to use.
+    PTKP = K(perm, perm);
+
+    A = PTKP(1:rank, 1:rank);
+    
+    [U,S,~] = svd(A,"vector");
+    
+    for i = 1 : rank
+        
+        singval = S(i);
+
+        if singval > 0
+            col = U(:,i);
+            Aplus = Aplus + (1.0 / singval) * (col * col.');
+        end
+
+    end
+
+    
+    R = PTKP(1:rank,:);
+
+    lognoise = learnInfo.hyp(5); %noise of Y
+    PreConInvRaw = (1 / exp(lognoise)^2)*eye(N*D*M*LForDecomp) - (1 / exp(lognoise)^4)*P'*R'*pinv((pinv(Aplus) + (1/exp(lognoise)^2) * (R * R')))*R*P;
+  
+    mat = pinv(Aplus) + 1/sigma * (R * R');
+    symmat = (mat + mat') / 2;
+    
+    logDetPreCon = trace(logm(symmat)) + trace(logm(Aplus)) + D*M*N*LForDecomp * log(sigma);
+        
+    
+    end
+
+
+
