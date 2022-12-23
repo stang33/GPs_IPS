9b97b412e30663a6b4b09f0bc7624cb2f11ae134
CharlesKulick1 <105407766+CharlesKulick1@users.noreply.github.com>
2022-12-22 20:43:26 -0800

    Delete Accel directory

--- Accel/IdentityPrecon.m
+++
@@ -1,6 +0,0 @@
-function [logDetPreCon, PreConInvRaw] = IdentityPrecon(learnInfo, LForDecomp, M, rangeOfI, jitter, KE, KA, sigma)
-
-    N = learnInfo.N;
-    D = learnInfo.d;
-    logDetPreCon = 0;
-    PreConInvRaw = eye(M*LForDecomp*N*D);
--- Accel/KForFullRExp52Omega.m
+++
@@ -1,5 +0,0 @@
-function out = KForFullRExp52Omega(x,y,omega,delta,n)
-
-    out = (1/n^2) * delta^2 * exp(-1 * omega * abs(x - y) * sqrt(5)) * (1 + sqrt(5)*abs(x - y)*omega + 5/3*abs(x - y)^2*omega^2);
-
-end
